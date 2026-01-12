d5caaf17babd87e914b8dbbe4f0d99c6b53bbd69
Reinier van der Leer <github@pwuts.nl>
2026-01-12 16:43:29 +0100

    update pyright to v1.1.408 and fix type error snapshots

--- typesafety/pyright/types/bigint.py
+++ typesafety/pyright/types/bigint.py
@@ -91,7 +91,7 @@ async def filtering(client: Prisma) -> None:
         },
     )
     await client.types.find_first(
-        where={  # E: Argument of type "dict[str, dict[str, tuple[Unknown, ...]]]" cannot be assigned to parameter "where" of type "TypesWhereInput | None" in function "find_first"
+        where={  # E: Argument of type "dict[str, dict[str, tuple[()]]]" cannot be assigned to parameter "where" of type "TypesWhereInput | None" in function "find_first"
             'bigint': {
                 'lte': tuple(),
             },
